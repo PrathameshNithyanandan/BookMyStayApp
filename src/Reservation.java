0545132042088feb2d6b33cc45d2e60a7a727126
Prathamesh Nithyanandan <71165940+PrathameshNithyanandan@users.noreply.github.com>
2026-03-10 10:02:29 +0530

    Add Reservation class for managing guest bookings

---
+++ src/Reservation.java
@@ -0,0 +1,21 @@
+public class Reservation {
+private String guestName;
+private String roomType;
+
+public Reservation(String guestName,String roomType) {
+this.guestName=guestName;
+this.roomType=roomType;
+}
+
+public String getGuestName() {
+return guestName;
+}
+
+public String getRoomType() {
+return roomType;
+}
+
+public void displayReservation() {
+System.out.println("Guest Name: "+guestName);
+System.out.println("Requested Room Type: "+roomType);
+}
